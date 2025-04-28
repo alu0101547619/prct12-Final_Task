7fbe8966bc77e7da1d4c02a5555f05e014708f75
Adrian Martin <alu0101547619@ull.edu.es>
2025-04-28 09:42:24 +0000

    Terminado jejej

---
+++ src/ejercicio_PE/funko.ts
@@ -0,0 +1,90 @@
+import { connect, model, Schema } from 'mongoose';
+import validator from 'validator';
+
+connect('mongodb://127.0.0.1:27017/funko_pops').then(() => {
+  console.log('Connected to the database');
+}).catch(() => {
+  console.log('Something went wrong when conecting to the database');
+});
+
+interface FunkoDocumentInterface extends Document {
+  ID: number,
+  name: string,
+  description: string,
+  type: 'Pop!' | 'Pop! Rides' | 'Vynil Soda' | 'Vynil Gold',
+  gender: 'Animación' | 'Películas y TV' | 'Videojuegos' | 'Deportes' | 'Música' | 'Anime',
+  theme: string,
+  num: number,
+  exclusive: boolean,
+  special_characteristics: string,
+  value: number
+}
+
+const NoteSchema = new Schema<FunkoDocumentInterface>({
+  ID: {
+    type: Number,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    validate: (value: string) => {
+			if (!value.match(/^[A-Z]/)) {
+				throw new Error('El nombre debe comenzar con mayúscula.');
+			} else if (!validator.default.isAlphanumeric(value)) {
+				throw new Error('El nombre solo puede contener caracteres alfanuméricos.');
+			}
+		}
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  type: {
+    type: String,
+    trim: true,
+    required: true,
+    enum: ['Pop!', 'Pop! Rides', 'Vynil Soda', 'Vynil Gold'],
+  },
+  gender: {
+    type: String,
+    trim: true,
+    required: true,
+    enum: ['Animación', 'Películas y TV', 'Videojuegos', 'Deportes', 'Música', 'Anime'],
+  },
+  theme: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: (value: string) => {
+			if (!value.match(/^[A-Z]/)) {
+				throw new Error('El nombre debe comenzar con mayúscula.');
+			} else if (!validator.default.isAlphanumeric(value)) {
+				throw new Error('El nombre solo puede contener caracteres alfanuméricos.');
+			}
+		}
+  },
+  num: {
+    type: Number,
+    required: true,
+  },
+  exclusive: {
+    type: Boolean,
+    required: true,
+  },
+  special_characteristics: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const Funko = model<FunkoDocumentInterface>('Funko', NoteSchema);
---
+++ src/ejercicio_PE/route.ts
@@ -0,0 +1,94 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { Funko } from './funko.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/funko', (req, res) => {
+  const note = new Funko(req.body);
+
+  note.save().then((funko) => {
+    res.status(201).send(funko);
+  }).catch((error) => {
+    res.status(400).send(error);
+  });
+});
+
+app.get('/funko', (req, res) => {
+  const filter = req.query.ID?{ID: req.query.ID.toString()}:{};
+
+  Funko.find(filter).then((funko) => {
+    if (funko.length !== 0) {
+      res.send(funko);
+    } else {
+      res.status(404).send();
+    }
+  }).catch(() => {
+    res.status(500).send();
+  });
+});
+
+app.patch('/funko', (req, res) => {
+  if (!req.query.ID) {
+    res.status(400).send({
+      error: 'A title must be provided in the query string',
+    });
+  } else if (!req.body) {
+    res.status(400).send({
+      error: 'Fields to be modified have to be provided in the request body',
+    });
+  } else {
+    const allowedUpdates = ['ID', 'name', 'description', 'type','gender', 'theme', 'num', 'exclusive', 'special_characteristics', 'value'];
+    const actualUpdates = Object.keys(req.body);
+    const isValidUpdate =
+      actualUpdates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidUpdate) {
+      res.status(400).send({
+        error: 'Update is not permitted',
+      });
+    } else {
+      Funko.findOneAndUpdate({ID: req.query.ID}, req.body, {
+        new: true,
+        runValidators: true,
+      }).then((funko) => {
+        if (!funko) {
+          res.status(404).send();
+        } else {
+          res.send(funko);
+        }
+      }).catch((error) => {
+        res.status(400).send(error);
+      });
+    }
+  }
+});
+
+app.delete('/funko', (req, res) => {
+  if (!req.query.ID) {
+    res.status(400).send({
+      error: 'A title must be provided',
+    });
+  } else {
+    Funko.findOneAndDelete({ID: req.query.ID.toString()}).then((funko) => {
+      if (!funko) {
+        res.status(404).send();
+      } else {
+        res.send(funko);
+      }
+    }).catch(() => {
+      res.status(400).send();
+    });
+  }
+});
+
+app.all('/{*splat}', (_, res) => {
+  res.status(501).send();
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
